d85477e407603c12fbc3e36753fba485d21d8fff
ABoCiDeErFoGe <shua365@aucklanduni.ac.nz>
2024-09-15 22:53:18 +1200

    added setCharacter method in chatHandler, and tested it on one of the scene

--- src/main/java/nz/ac/auckland/se206/ChatHandler.java
+++ src/main/java/nz/ac/auckland/se206/ChatHandler.java
@@ -1,3 +1,45 @@
 package nz.ac.auckland.se206;
 
-public class ChatHandler {}
+import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionRequest;
+import nz.ac.auckland.apiproxy.chat.openai.ChatCompletionResult;
+import nz.ac.auckland.apiproxy.chat.openai.ChatMessage;
+import nz.ac.auckland.apiproxy.chat.openai.Choice;
+import nz.ac.auckland.apiproxy.config.ApiProxyConfig;
+import nz.ac.auckland.apiproxy.exceptions.ApiProxyException;
+import nz.ac.auckland.se206.prompts.PromptEngineering;
+
+public class ChatHandler {
+
+  private static ChatCompletionRequest chatCompletionRequest;
+
+  ChatHandler() {}
+
+  public static String setCharacter(String characterID) {
+    try {
+      ApiProxyConfig config = ApiProxyConfig.readConfig();
+      chatCompletionRequest =
+          new ChatCompletionRequest(config)
+              .setN(1)
+              .setTemperature(0.5)
+              .setTopP(0.5)
+              .setMaxTokens(100);
+      return runGpt(new ChatMessage("system", PromptEngineering.getPrompt(characterID)));
+    } catch (ApiProxyException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public static String runGpt(ChatMessage msg) throws ApiProxyException {
+    chatCompletionRequest.addMessage(msg);
+    try {
+      ChatCompletionResult chatCompletionResult = chatCompletionRequest.execute();
+      Choice result = chatCompletionResult.getChoices().iterator().next();
+      chatCompletionRequest.addMessage(result.getChatMessage());
+      return result.getChatMessage().getContent();
+    } catch (ApiProxyException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
