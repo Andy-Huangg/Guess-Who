115012e69398e2b929ff8db4cd3a7989a0bd9e21
ABoCiDeErFoGe <shua365@aucklanduni.ac.nz>
2024-09-17 19:06:44 +1200

    edit on the controller to respond to client request

--- src/main/java/nz/ac/auckland/se206/controllers/GuessController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GuessController.java
@@ -4,12 +4,39 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+import nz.ac.auckland.se206.ChatHandler;
 
 public class GuessController {
 
-  @FXML Label timeLabel, resultLabel, explainLabel;
+  @FXML Label timeLabel, resultLabel, explainLabel, suspectSelectedLabel;
   @FXML TextArea answerText;
   @FXML Button SubmitBtn;
+  @FXML Pane suspectSelectedPane;
 
-  public void initialize() {}
+  String suspectSelected;
+
+  public void initialize() {
+    // load the prompt at the start;
+    Thread loadingPrompt =
+        new Thread(
+            () -> {
+              ChatHandler.setCharacter("owner");
+            });
+    loadingPrompt.start();
+  }
+
+  @FXML
+  private void getSuspect(MouseEvent event) {
+    ImageView selected = (ImageView) event.getSource();
+    enableSuspectSelectedPane(selected.getId());
+  }
+
+  private void enableSuspectSelectedPane(String id) {
+    suspectSelectedPane.setVisible(true);
+    suspectSelected = id;
+    suspectSelectedLabel.setText("Well done... Why do you think " + id + " is the theif?");
+  }
 }
