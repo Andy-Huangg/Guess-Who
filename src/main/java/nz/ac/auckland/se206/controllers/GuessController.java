68276710a3d974ab418c05fd105a710c09de69ab
ABoCiDeErFoGe <shua365@aucklanduni.ac.nz>
2024-09-17 13:49:46 +1200

    fxml finished, made a start on controller

--- src/main/java/nz/ac/auckland/se206/controllers/GuessController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/GuessController.java
@@ -1,5 +1,15 @@
 package nz.ac.auckland.se206.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+
 public class GuessController {
+
+  @FXML Label timeLabel, resultLabel, explainLabel;
+  @FXML TextArea answerText;
+  @FXML Button SubmitBtn;
+
   public void initialize() {}
 }
