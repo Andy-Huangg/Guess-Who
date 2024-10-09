325ea45ae65a8e6f702384df1a23330d3c3b53eb
ABoCiDeErFoGe <shua365@aucklanduni.ac.nz>
2024-10-10 02:33:13 +1300

    initial preparation for the controller is complete

--- src/main/java/nz/ac/auckland/se206/controllers/OpeningController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/OpeningController.java
@@ -2,126 +2,141 @@ package nz.ac.auckland.se206.controllers;
 
 import javafx.animation.FadeTransition;
 import javafx.animation.TranslateTransition;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 public class OpeningController {
 
   @FXML ImageView block1;
-  @FXML ImageView block2;
-  @FXML ImageView block3;
-  @FXML ImageView block4;
-  @FXML ImageView mouseImage;
-  @FXML Label nameLabel;
-  @FXML Label dialogLabel;
-  @FXML Rectangle nameRectangle;
-  @FXML Rectangle dialogRectangle;
-  @FXML Rectangle coverRectangle;
+  @FXML Label block1Dialog1;
+  @FXML Label block1Dialog2;
 
-  ImageView currentBlock;
+  @FXML ImageView block2;
+  @FXML Label block2Dialog1;
+  @FXML Label block2Dialog2;
+
+  @FXML ImageView block3;
+  @FXML Label block3Dialog1;
+  @FXML Label block3Dialog2;
+
+  @FXML ImageView block4;
+  @FXML Label block4Dialog1;
+  @FXML Label block4Dialog2;
+
+  @FXML ImageView block5;
+  @FXML Label block5Dialog1;
+  @FXML Label block5Dialog2;
+
+  @FXML ImageView block6;
+  @FXML Label block6Dialog1;
+  @FXML Label block6Dialog2;
+
+  @FXML ImageView block7;
+  @FXML Label block7Dialog1;
+  @FXML Label block7Dialog2;
+
+  Thread openingThread;
 
   TranslateTransition blockAnimation = new TranslateTransition();
   int clickCount = 0;
   double opacity = 0;
 
-  FadeTransition coverAnimation = new FadeTransition();
+  FadeTransition fadeIn = new FadeTransition();
+  FadeTransition fadeOut = new FadeTransition();
 
-  String[] dialogArray = {
-    "",
-    "You must be the detective from PI Master. I’m Hawthorne, the owner of this house.",
-    " Come on in, I’ll tell you what happened.",
-    "",
-    " You see, I have got a gala hosting tonight, and 3 of my guests are here already.",
-    " I left at around 2:20pm for some party stuff. 30 mins later when I got back, I saw my safe"
-        + " opened and the jewellery was missing.",
-    "",
-    "That \"Sapphire of Versailles\", was supposed to be present tonight. It’s a priceless family"
-        + " heirloom of mine.",
-    "Shame to say that I forgot to lock the safe last night, but luckily I got you here.",
-    "",
-    "There we are, the study room, where I kept my jewellery. the thief must have left some clues"
-        + " here.",
-    "Good luck detective, found the thief among them three. I’ll see you at 4pm. Don’t let me down."
-  };
+  FadeTransition textFadeIn = new FadeTransition();
+  FadeTransition textFadeOut = new FadeTransition();
+
+  TranslateTransition pullFromLeft = new TranslateTransition();
+  TranslateTransition pullFromRight = new TranslateTransition();
+
+  TranslateTransition textGoRight = new TranslateTransition();
+  TranslateTransition textGoLeft = new TranslateTransition();
 
   public void initialize() {
 
-    // setting animation properties
-    coverAnimation.setFromValue(0);
-    coverAnimation.setToValue(1);
-    coverAnimation.setByValue(0.05);
-    coverAnimation.setNode(coverRectangle);
-    coverAnimation.setDuration(Duration.millis(2000));
+    fadeIn.setFromValue(0);
+    fadeIn.setToValue(0.55);
+    fadeIn.setDuration(Duration.millis(1000));
 
-    currentBlock = block1;
-    blockAnimation.setByX(8);
-    blockAnimation.setAutoReverse(true);
-    blockAnimation.setDuration(Duration.millis(150));
-    blockAnimation.setCycleCount(2);
-    blockAnimation.setNode(currentBlock);
+    fadeOut.setFromValue(0.55);
+    fadeOut.setToValue(0);
+    fadeOut.setDuration(Duration.millis(500));
+
+    textFadeIn.setToValue(1);
+    textFadeIn.setDuration(Duration.millis(500));
+
+    textFadeOut.setFromValue(1);
+    textFadeOut.setToValue(0);
+    textFadeOut.setDuration(Duration.millis(300));
+
+    pullFromLeft.setByX(-10);
+    pullFromLeft.setDuration(Duration.millis(1600));
+    pullFromRight.setByX(50);
+    pullFromRight.setDuration(Duration.millis(1600));
+
+    textGoLeft.setByX(50);
+    textGoLeft.setDuration(Duration.millis(8000));
+
+    textGoRight.setByY(50);
+    textGoRight.setDuration(Duration.millis(8000));
+
+    openingThread =
+        new Thread(
+            () -> {
+              Platform.runLater(
+                  () -> {
+                    textFadeIn(block1Dialog1);
+                    imageFadeIn(block1);
+                    imagePull(true, block1);
+                    textGo(true, block1Dialog1);
+                    textFadeIn(block1Dialog2);
+                    textGo(true, block1Dialog2);
+                  });
+            });
+    openingThread.start();
   }
 
-  @FXML
-  public void progress() {
-    mouseImage.setVisible(false);
-    if (clickCount == 1) { // enableing the chatbox related nodes
-      nameLabel.setVisible(true);
-      nameRectangle.setVisible(true);
-      dialogLabel.setVisible(true);
-      dialogRectangle.setVisible(true);
-    }
-    // prevent further click when all dialog appeared
-    if (clickCount > 12) {
-      return;
-    }
-    System.out.println(clickCount);
-    updateDialog();
-    showBlock();
-    clickCount++;
+  public void imageFadeIn(ImageView temp) {
+    fadeIn.setNode(temp);
+    fadeIn.play();
   }
 
-  public void updateDialog() {
-    if (clickCount == 12) {
-      transitionToGame();
-      return;
-    }
-    if (clickCount == 2) {
-      nameLabel.setText("Hawthorne");
-    }
-    dialogLabel.setText(dialogArray[clickCount]);
+  public void imageFadeOut(ImageView temp) {
+    fadeOut.setNode(temp);
+    fadeOut.play();
   }
 
-  public void showBlock() {
-    // play the correspond animation accroding to clue count
-    if (clickCount == 0) {
-      blockAnimation.play();
-      currentBlock.setVisible(true);
-      currentBlock = block2;
-      blockAnimation.setNode(currentBlock);
-    }
-    if (clickCount == 3) {
-      blockAnimation.play();
-      currentBlock.setVisible(true);
-      currentBlock = block3;
-      blockAnimation.setNode(currentBlock);
-    }
-    if (clickCount == 6) {
-      blockAnimation.play();
-      currentBlock.setVisible(true);
-      currentBlock = block4;
-      blockAnimation.setNode(currentBlock);
-    }
-    if (clickCount == 9) {
-      blockAnimation.play();
-      currentBlock.setVisible(true);
+  public void imagePull(boolean isLeft, ImageView temp) {
+    if (isLeft) {
+      pullFromLeft.setNode(temp);
+      pullFromLeft.play();
+    } else {
+      pullFromRight.setNode(temp);
+      pullFromRight.play();
     }
   }
 
-  public void transitionToGame() {
-    coverAnimation.play();
-    // to mainlayour controller
+  public void textFadeIn(Label temp) {
+    textFadeIn.setNode(temp);
+    textFadeIn.play();
+  }
+
+  public void textFadeOut(Label temp) {
+    textFadeOut.setNode(temp);
+    textFadeOut.play();
+  }
+
+  public void textGo(boolean isLeft, Label temp) {
+    if (isLeft) {
+      textGoLeft.setNode(temp);
+      textGoLeft.play();
+    } else {
+      textGoRight.setNode(temp);
+      textGoRight.play();
+    }
   }
 }
