df9f9bae9a5959da18f08db04cdf563507d6e40c
ABoCiDeErFoGe <shua365@aucklanduni.ac.nz>
2024-10-10 04:00:21 +1300

    complete the main timeline of the opening, still need adjustment

--- src/main/java/nz/ac/auckland/se206/controllers/OpeningController.java
+++ src/main/java/nz/ac/auckland/se206/controllers/OpeningController.java
@@ -44,12 +44,6 @@ public class OpeningController {
   int clickCount = 0;
   double opacity = 0;
 
-  FadeTransition fadeIn = new FadeTransition();
-  FadeTransition fadeOut = new FadeTransition();
-
-  FadeTransition textFadeIn = new FadeTransition();
-  FadeTransition textFadeOut = new FadeTransition();
-
   TranslateTransition pullFromLeft = new TranslateTransition();
   TranslateTransition pullFromRight = new TranslateTransition();
 
@@ -58,30 +52,99 @@ public class OpeningController {
 
   public void initialize() {
 
-    fadeIn.setFromValue(0);
-    fadeIn.setToValue(0.55);
-    fadeIn.setDuration(Duration.millis(1000));
-
-    fadeOut.setFromValue(0.55);
-    fadeOut.setToValue(0);
-    fadeOut.setDuration(Duration.millis(500));
-
-    textFadeIn.setToValue(1);
-    textFadeIn.setDuration(Duration.millis(500));
-
-    textFadeOut.setFromValue(1);
-    textFadeOut.setToValue(0);
-    textFadeOut.setDuration(Duration.millis(300));
-
     openingThread =
         new Thread(
             () -> {
               try {
+                // block1
                 textFadeIn(block1Dialog1);
                 imageFadeIn(block1);
                 imagePull(true, block1, 1500);
-                textGo(true, block1Dialog1, 1500);
+                textGo(false, block1Dialog1, 1300);
                 Thread.currentThread().sleep(500);
+                textFadeIn(block1Dialog2);
+                textGo(true, block1Dialog2, 800);
+                Thread.currentThread().sleep(1000);
+                textFadeOut(block1Dialog1);
+                textFadeOut(block1Dialog2);
+                imageFadeOut(block1);
+                // end of block 1
+                Thread.currentThread().sleep(10);
+
+                // block 2
+                imageFadeIn(block2);
+                textFadeIn(block2Dialog1);
+                imagePull(false, block2, 2000);
+                textGo(false, block2Dialog1, 2000);
+                Thread.currentThread().sleep(1000);
+                textFadeIn(block2Dialog2);
+                textGo(true, block2Dialog2, 1000);
+                Thread.currentThread().sleep(1100);
+                textFadeOut(block2Dialog1);
+                textFadeOut(block2Dialog2);
+                imageFadeOut(block2);
+                Thread.currentThread().sleep(10);
+
+                imageFadeIn(block3);
+                textFadeIn(block3Dialog1);
+                imagePull(true, block3, 1500);
+                textGo(false, block3Dialog1, 800);
+                Thread.currentThread().sleep(350);
+                textFadeIn(block3Dialog2);
+                textGo(true, block3Dialog2, 600);
+                Thread.currentThread().sleep(950);
+                textFadeOut(block3Dialog1);
+                textFadeOut(block3Dialog2);
+                imageFadeOut(block3);
+                Thread.currentThread().sleep(10);
+
+                imageFadeIn(block4);
+                textFadeIn(block4Dialog1);
+                textGo(false, block4Dialog1, 1500);
+                imageDown(block4);
+                Thread.currentThread().sleep(1000);
+                textFadeIn(block4Dialog2);
+                textGo(true, block4Dialog2, 1000);
+                Thread.currentThread().sleep(1750);
+                textFadeOut(block4Dialog1);
+                textFadeOut(block4Dialog2);
+                imageFadeOut(block4);
+                Thread.currentThread().sleep(10);
+
+                imageFadeIn(block5);
+                textFadeIn(block5Dialog1);
+                textGo(false, block5Dialog1, 1500);
+                imageUp(block5);
+                Thread.currentThread().sleep(1000);
+                textFadeIn(block5Dialog2);
+                textGo(true, block5Dialog2, 1000);
+                Thread.currentThread().sleep(1750);
+                textFadeOut(block5Dialog1);
+                textFadeOut(block5Dialog2);
+                imageFadeOut(block5);
+                Thread.currentThread().sleep(10);
+
+                imageFadeIn(block6);
+                textFadeIn(block6Dialog1);
+                textGo(false, block6Dialog1, 1500);
+                imageDown(block6);
+                Thread.currentThread().sleep(1000);
+                textFadeIn(block6Dialog2);
+                textGo(true, block6Dialog2, 1000);
+                Thread.currentThread().sleep(1750);
+                textFadeOut(block6Dialog1);
+                textFadeOut(block6Dialog2);
+                imageFadeOut(block6);
+                Thread.currentThread().sleep(10);
+
+                imageFadeIn(block7);
+                textFadeIn(block7Dialog1);
+                textGo(false, block7Dialog1, 1500);
+                Thread.currentThread().sleep(1000);
+                textFadeIn(block7Dialog2);
+                textGo(true, block7Dialog2, 1000);
+                Thread.currentThread().sleep(1750);
+                // transit to black screen
               } catch (Exception e) {
               }
             });
@@ -91,6 +154,9 @@ public class OpeningController {
   public void imageFadeIn(ImageView temp) {
     Platform.runLater(
         () -> {
+          FadeTransition fadeIn = new FadeTransition();
+          fadeIn.setToValue(0.60);
+          fadeIn.setDuration(Duration.millis(750));
           fadeIn.setNode(temp);
           fadeIn.play();
         });
@@ -99,6 +165,9 @@ public class OpeningController {
   public void imageFadeOut(ImageView temp) {
     Platform.runLater(
         () -> {
+          FadeTransition fadeOut = new FadeTransition();
+          fadeOut.setToValue(0);
+          fadeOut.setDuration(Duration.millis(500));
           fadeOut.setNode(temp);
           fadeOut.play();
         });
@@ -127,6 +196,9 @@ public class OpeningController {
   public void textFadeIn(Label temp) {
     Platform.runLater(
         () -> {
+          FadeTransition textFadeIn = new FadeTransition();
+          textFadeIn.setToValue(1);
+          textFadeIn.setDuration(Duration.millis(500));
           textFadeIn.setNode(temp);
           textFadeIn.play();
         });
@@ -135,6 +207,9 @@ public class OpeningController {
   public void textFadeOut(Label temp) {
     Platform.runLater(
         () -> {
+          FadeTransition textFadeOut = new FadeTransition();
+          textFadeOut.setToValue(0);
+          textFadeOut.setDuration(Duration.millis(500));
           textFadeOut.setNode(temp);
           textFadeOut.play();
         });
@@ -158,5 +233,21 @@ public class OpeningController {
             textGoRight.play();
           });
     }
+  }
+
+  public void imageUp(ImageView temp) {
+    TranslateTransition goUp = new TranslateTransition();
+    goUp.setDuration(Duration.millis(3000));
+    goUp.setByY(2800 / 160);
+    goUp.setNode(temp);
+    goUp.play();
+  }
+
+  public void imageDown(ImageView temp) {
+    TranslateTransition goDown = new TranslateTransition();
+    goDown.setDuration(Duration.millis(2800));
+    goDown.setByY(-1 * 2800 / 160);
+    goDown.setNode(temp);
+    goDown.play();
   }
 }
